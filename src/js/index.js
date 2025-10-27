4e4aebccc50a019caa8b1180729f6cefe7f0e8c6
Joao Vitor <joao62957@gmail.com>
2025-10-27 14:21:00 -0300

    primeiro commit

---
+++ src/js/index.js
@@ -0,0 +1,60 @@
+// Objetivo:
+// Enviar um texto de um formulário para uma API do n8n e exibir o resultado o código html, css e colocar a animação no fundo da tela do site.
+
+// Passos:
+// 1. No JavaScript, pegar o evento de submit do formulário para evitar o recarregamento da página.
+// 2. Obter o valor digitado pelo usuário no campo de texto.
+// 3. Exibir um indicador de carregamento enquanto a requisição está sendo processada.
+// 4. Fazer uma requisição HTTP (POST) para a API do n8n, enviando o texto do formulário no corpo da requisição em formato JSON.
+// 5. Receber a resposta da API do n8n (esperando um JSON com o código HTML/CSS do background.
+// 6. Se a resposta for válida, exibir o código HTML/CSS retornado na tela:
+//    - Mostrar o HTML gerado em uma área de preview.
+//    - Inserir o CSS retornado dinamicamente na página para aplicar o background.
+// 7. Remover o indicador de carregamento após o recebimento da resposta.
+
+function setLoading(isLoading) {
+    const btnSpan = document.getElementById('generate-btn');
+
+    if(isLoading){
+        btnSpan.innerHTML = 'Gerando Background...';
+    }else{
+        btnSpan.innerHTML = 'Gerar Background Mágico';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    // 1. No JavaScript, pegar o evento de submit do formulário para evitar o recarregamento da página.
+
+    const form = document.querySelector('.form-group');
+    const textArea = document.getElementById('description'); 
+
+    form.addEventListener('submit', async function(event) {
+        event.preventDefault(); 
+
+        // 2. Obter o valor digitado pelo usuário no campo de texto.
+        const descrpition = textArea.value.trim();
+
+        if(!descrpition){
+            return;
+        }
+        
+        // 3. Exibir um indicador de carregamento enquanto a requisição está sendo processada.
+        setLoading(true);
+
+        // 4. Fazer uma requisição HTTP (POST) para a API do n8n, enviando o texto do formulário no corpo da requisição em formato JSON.
+        try {
+
+            const response = await fetch('https://jyoao.app.n8n.cloud/webhook-test/Background-generetion', {
+                method: 'POST',
+                headers: {'Content-type': 'application/json'},
+                body: JSON.stringify({descrpition})
+            })
+
+        }catch{
+        
+        }finally{
+
+        }
+
+    });
+});
